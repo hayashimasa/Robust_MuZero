f18a9cdf3d0c6b9535e428978529164ba61c04eb
Masahiro Hayashi <41661297+hayashimasa@users.noreply.github.com>
2021-03-13 18:16:35 +0100

    Add robust MCTS

--- recurrent/self_play.py
+++ recurrent/self_play.py
@@ -258,6 +258,131 @@ class SelfPlay:
 
         return action
 
+RobustMCTS(MCTS):
+
+    def __init__(self, config):
+        super(RobustMCTS, self).__init__(config)
+
+    def run(
+        self,
+        model,
+        observation,
+        legal_actions,
+        to_play,
+        add_exploration_noise,
+        override_root_with=None,
+    ):
+        """At the root of the search tree we use the representation function
+        to obtain a hidden state given the current observation.
+        We then run a Monte Carlo Tree Search using only action sequences and
+        the model learned by the network.
+        """
+        if override_root_with:
+            root = override_root_with
+            root_predicted_value = None
+        else:
+            root = Node(0)
+            observation = (
+                torch.tensor(observation)
+                .float()
+                .unsqueeze(0)
+                .to(next(model.parameters()).device)
+            )
+            (
+                root_predicted_value,
+                reward,
+                policy_logits,
+                encoded_state,
+            ) = model.initial_inference(observation)
+            root_predicted_value = network.support_to_scalar(
+                root_predicted_value, self.config.support_size
+            ).item()
+            reward = network.support_to_scalar(reward, self.config.support_size).item()
+            assert (
+                legal_actions
+            ), f"Legal actions should not be an empty array. Got {legal_actions}."
+            assert set(legal_actions).issubset(
+                set(self.config.action_space)
+            ), "Legal actions should be a subset of the action space."
+            root.expand(
+                legal_actions,
+                to_play,
+                reward,
+                policy_logits,
+                encoded_state,
+                encoded_state
+            )
+
+        if add_exploration_noise:
+            root.add_exploration_noise(
+                dirichlet_alpha=self.config.root_dirichlet_alpha,
+                exploration_fraction=self.config.root_exploration_fraction,
+            )
+
+        min_max_stats = MinMaxStats()
+
+        max_tree_depth = 0
+        for _ in range(self.config.num_simulations):
+            virtual_to_play = to_play
+            node = root
+            search_path = [node]
+            current_tree_depth = 0
+
+            while node.expanded():
+                current_tree_depth += 1
+                action, node = self.select_child(node, min_max_stats)
+                search_path.append(node)
+
+                # Players play turn by turn
+                if virtual_to_play + 1 < len(self.config.players):
+                    virtual_to_play = self.config.players[virtual_to_play + 1]
+                else:
+                    virtual_to_play = self.config.players[0]
+
+            # Inside the search tree we use the dynamics function to obtain
+            # the next hidden state given an action and the previous hidden
+            # state
+            parent = search_path[-2]
+            K = 5
+            value = None
+            reward = numpy.inf
+            policy_logits = None
+            encoded_state = None
+            hidden_state = None
+            for _ in range(K):
+                v, r, p, s, h = model.recurrent_inference(
+                    parent.encoded_state,
+                    torch.tensor([[action]]).to(parent.encoded_state.device),
+                    parent.hidden_state
+                )
+                if r < reward:
+                    value = v
+                    reward = r
+                    policy_logits = p
+                    encoded_state = s
+                    hidden_state = h
+
+            value = network.support_to_scalar(value, self.config.support_size).item()
+            reward = network.support_to_scalar(reward, self.config.support_size).item()
+
+            node.expand(
+                self.config.action_space,
+                virtual_to_play,
+                reward,
+                policy_logits,
+                encoded_state,
+                hidden_state
+            )
+
+            self.backpropagate(search_path, value, virtual_to_play, min_max_stats)
+
+            max_tree_depth = max(max_tree_depth, current_tree_depth)
+
+        extra_info = {
+            "max_tree_depth": max_tree_depth,
+            "root_predicted_value": root_predicted_value,
+        }
+        return root, extra_info
 
 # Game independent
 class MCTS:
